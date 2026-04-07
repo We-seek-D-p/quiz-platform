c80f391d076f1a3fc712b62f4655493dcca0c37f
Константин Макаренко <kostamak@MacBook-Pro-Konstantin-2.local>
2026-04-07 20:28:02 +0300

    feat(management): add sessions service and fix sessions repo

--- apps/management/src/quiz_management/repositories/session_repositories.py
+++ apps/management/src/quiz_management/repositories/session_repositories.py
@@ -1,13 +1,17 @@
+from typing import TypeVar
+
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from quiz_management.models.session import GameSession, SessionParticipant
+
+TSession = TypeVar("TSession", GameSession, SessionParticipant)
 
 
 class SessionRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    async def create_session(self, session: GameSession | SessionParticipant) -> GameSession:
+    async def save_session(self, session: TSession) -> TSession:
         self.db.add(session)
         await self.db.commit()
         await self.db.refresh(session)
---
+++ apps/management/src/quiz_management/services/session.py
@@ -0,0 +1,10 @@
+from repositories.session_repositories import SessionRepository, TSession
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+
+class SessionService:
+    def __init__(self, db: AsyncSession):
+        self.repository = SessionRepository(db)
+
+    async def create_session(self, data: TSession) -> TSession:
+        return await self.repository.save_session(data)
